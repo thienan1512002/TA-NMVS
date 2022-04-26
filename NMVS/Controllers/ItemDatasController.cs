e318f8dc043db0eb642316db292344a78cb66682
thienan1512002 <72856151+thienan1512002@users.noreply.github.com>
2022-04-26 08:59:55 +0700

    Update R ItemData

--- NMVS/Controllers/ItemDatasController.cs
+++ NMVS/Controllers/ItemDatasController.cs
@@ -8,15 +8,18 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NMVS.Models;
+using AspNetCoreHero.ToastNotification.Abstractions;
 
 namespace NMVS.Controllers
 {
     public class ItemDatasController : Controller
     {
         private readonly InItemData _services;
-        public ItemDatasController(InItemData services)
+        private readonly INotyfService _notyf;
+        public ItemDatasController(InItemData services, INotyfService notyf)
         {
             _services = services;
+            _notyf = notyf;
         }
 
         public IActionResult Index()
@@ -53,10 +56,49 @@ namespace NMVS.Controllers
             {
                 if (await _services.AddItemData(itemData))
                 {
+                    _notyf.Success("Item Data Created Successfully", 5);
                     return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "There are already Item that have code : " + itemData.ItemNo + " with name : " + itemData.ItemName;
                 }
             }
 
+            return View(itemData);
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            ItemData model = await _services.GetItemDataById(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(ItemData itemData)
+        {
+            if (ModelState.IsValid)
+            {
+                if (itemData.Active == null)
+                {
+                    itemData.Active = false;
+                }
+                if (itemData.Flammable == null)
+                {
+                    itemData.Flammable = false;
+                }
+                if (await _services.UpdateItemData(itemData))
+                {
+                    _notyf.Success("Item Data Updated Successfully", 5);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "There are already Item that have code : " + itemData.ItemNo + " with name : " + itemData.ItemName;
+                }
+            }
             return View(itemData);
         }
     }
